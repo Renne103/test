eb1fb1a9376de1fe5d24f8174edb950eafde3381
RennI <98748434+Renne103@users.noreply.github.com>
2024-06-27 14:16:45 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const http = require("http");
  const proxy = http.createServer();
 
 proxy.on("request", (clientRequest, proxyResponse) => {
-
+    proxyResponse.setHeader('Access-Control-Allow-Origin', '*'); 
+    proxyResponse.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); 
+    proxyResponse.setHeader('Access-Control-Allow-Headers', 'Content-Type'); 
+    proxyResponse.setHeader('Access-Control-Allow-Credentials', true);
 const proxyRequest = http.request({ host: mainServer.host, path: clientRequest.url, method: clientRequest.method, headers: clientRequest.headers, });
 
 proxyRequest.on("response", (mainServerResponse) => { 
